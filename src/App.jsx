9c5bd4d89524b0a267ea394838d7f6bcc45bce69
Pablo Henrique Fragoso <ti2@ciclocairu.com.br>
2025-05-31 09:51:12 -0400

    base de estilos do projeto,para estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react'
+import './App.css'
+import './global.css'
+
+export function App() {
+
+  const [prompt, setPrompt] = useState('');
+  const [resposta, setResposta] = useState('');
+  const [carregando, setCarregando] = useState(false);
+
+  const handleSubmit = async(hS) =>{
+    hS.preventDefault();
+    setCarregando(true);
+    setResposta('');
+
+    try {
+      const res = await axios.post('https://localhost:4000', {prompt});
+      setResposta(res.data.resposta);
+    } catch (error) {
+      setResposta('Deu ruim, erro ao obter resposta da OpenAI')
+      console.error(error)
+    }
+    setCarregando(false);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <h1 className='title' >Gerador de texto - Jarbas</h1>
+
+      <form onSubmit={handleSubmit}>
+        <textarea 
+          placeholder="Como posso ajudar?" 
+          value={prompt} 
+          onChange={(hS) => setPrompt(hS.target.value)}
+         />
+
+         <button 
+         className='btn'
+          type='submit' 
+          disabled={carregando}>
+            {carregando ? 'Gerando...' : 'Enviar'}</button>
+      </form>
+      {resposta && (
+        <div className='resposta'>
+          <h2>Resposta</h2>
+          <p>Resposta</p>
+        </div>
+      )}
+    </div>
   )
 }
 
-export default App
